aa4c5af281a09baa1255304f9f1263af91d5a798
Jacob Coble <JCoble@lbl.gov>
2017-01-18 17:05:50 -0800

    flip reversed features

--- app/RailView/RailFeature.js
+++ app/RailView/RailFeature.js
@@ -15,15 +15,33 @@ export default class RailFeature extends React.Component {
             color
         } = feature;
 
+        const pointitude = 100;
+        const width = Math.max(end - start, pointitude);
+        const tail = width - pointitude > pointitude;
+        const flip = {
+            toString: () => {
+                if (!feature.forward) return `translate(${width}, 0) scale(-1,1)`;
+
+                return '';
+            }
+        };
+
         return (
             <g>
                 <path
-                    d={`M ${start}, 0 L ${end}, 0 L ${end}, ${height} L ${start}, ${height} Z`}
+                    d={`M 0, 0
+                        L ${width - pointitude}, 0
+                        L ${width}, ${height/2}
+                        L ${width - pointitude}, ${height}
+                        L 0, ${height}
+                        L ${(tail) ? pointitude : 0}, ${height/2}
+                        Z`}
                     fill={color}
                     stroke={'black'}
                     strokeWidth={'.25px'}
                     vectorEffect={'non-scaling-stroke'}
                     style={{vectorEffect: 'non-scaling-stroke'}}
+                    transform={`translate(${start}, 0) ${flip}`}
                 />
             </g>
         );
